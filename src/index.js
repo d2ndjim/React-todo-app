5d9cc8cb7e4539905019f57b3ffa3baf46080c79
Lekan <jimoridwanlekan@gmail.com>
2022-05-31 12:40:32 +0100

    add routing to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './classBased/App.css';
-import TodoContainer from './classBased/components/TodoContainer';
+import "./functionBased/App.css";
+import { BrowserRouter } from "react-router-dom";
+import TodoContainer from './functionBased/components/TodoContainer';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
